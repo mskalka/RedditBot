01022b6674fe66af1898ab8f073cb00b932850b8
Michael Skalka <michaelskalka@gmail.com>
2017-01-12 15:23:42 -0500

    First Commit

---
+++ Bot.py
@@ -0,0 +1,15 @@
+import praw
+
+reddit = praw.Reddit(client_id='',
+                     client_secret='',
+                     password='',
+                     user_agent='',
+                     username='')
+
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
