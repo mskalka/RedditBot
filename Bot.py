50e5120c73b32e3572fd4e77193223579a96ef9d
Michael <michaelskalka@gmail.com>
2017-01-14 01:33:54 -0500

    nothing major

--- Bot.py
+++ Bot.py
@@ -1,15 +1,34 @@
 import praw
+import time
 
-reddit = praw.Reddit(client_id='',
-                     client_secret='',
-                     password='',
-                     user_agent='',
-                     username='')
+
+YESTERDAY = time.time() - 86400  # 60*60*24 seconds before right now
+search_term = 'GGG'
 
 
 def main():
 
-    pass
+    r = praw.Reddit('orangebot')
+    sub = r.subreddit('pathofexile')
+
+    count = 0
+
+    for post in get_todays(sub):
+        for comment in post.comments:
+            if search_term in comment.body:
+                count += 1
+
+    print(count)
+
+
+def get_todays(subreddit):
+    result = []
+    for post in subreddit.new(limit=None):
+        if post.created_utc > YESTERDAY:
+            result.append(post)
+        else:
+            continue
+    return result
 
 if __name__ == "__main__":
     main()
---
+++ README.md
@@ -0,0 +1,7 @@
+Reddit Word Finder
+=============
+Playing around with the Reddit API using everyone's favorite Python Reddit API Wrapper: PRAW
+Goal right now is to count the number of times reddit users use a certain term in their
+comments each day.
+
+Result: It's really rather slow due to API limitations. 
